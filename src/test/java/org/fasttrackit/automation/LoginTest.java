3d5a1fa459580f637962a07ee17cd10aeabc0a03
BarraReka <rekabarra@gmail.com>
2017-05-04 21:41:51 +0300

    r

--- src/test/java/org/fasttrackit/automation/LoginTest.java
+++ src/test/java/org/fasttrackit/automation/LoginTest.java
@@ -1,11 +1,50 @@
 package org.fasttrackit.automation;
 import org.fasttrackit.util.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class LoginTest extends TestBase{
 
     @Test
     public void validLoginTest() {
-        System.out.println("ready");
+        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+
+        driver.findElement(By.name("username")).sendKeys("eu@fast.com");
+
+        WebElement passwordElement = driver.findElement(By.id("password"));
+        passwordElement.sendKeys("eu.passrererere");
+        passwordElement.clear();
+        passwordElement.sendKeys("eu.pass");
+
+        WebElement loginBtn = driver.findElement(By.id("loginButton"));
+        loginBtn.click();
+
+        WebElement logoutBtn = driver.findElement(By.linkText("Logout"));
+        logoutBtn.click();
     }
+
+    @Test
+    public void invalidUserAndPasswordTest(){
+
+        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+
+        driver.findElement(By.name("username")).sendKeys("wrong@user");
+        driver.findElement(By.id("password")).sendKeys("wrong.pass");
+        driver.findElement(By.id("loginButton")).click();
+
+        WebElement errorMsg = driver.findElement(By.className("error-msg"));
+        String message = errorMsg.getText();
+        System.out.println();
+
+       assertThat(message, is ("Invalid user or password!"));
+
+    }
+
+
+
 }
