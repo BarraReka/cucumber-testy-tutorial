d23a0d70137ce38d2f67e7778fbf5add276679ed
BarraReka <rekabarra@gmail.com>
2017-05-04 20:03:04 +0300

    r

---
+++ src/test/java/org/fasttrackit/automation/LoginTest.java
@@ -0,0 +1,11 @@
+package org.fasttrackit.automation;
+import org.fasttrackit.util.TestBase;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase{
+
+    @Test
+    public void validLoginTest() {
+        System.out.println("ready");
+    }
+}
