78eef64903791276eeb440af9ad647f270e78b11
BarraReka <rekabarra@gmail.com>
2017-06-15 16:19:15 +0300

    r

--- src/test/java/org/fasttrackit/automation/ExamplesTest.java
+++ src/test/java/org/fasttrackit/automation/ExamplesTest.java
@@ -1,7 +1,11 @@
 package org.fasttrackit.automation;
 
+import com.sdl.selenium.web.SearchType;
 import com.sdl.selenium.web.WebLocator;
 import com.sdl.selenium.web.form.CheckBox;
+import com.sdl.selenium.web.table.Cell;
+import com.sdl.selenium.web.table.Row;
+import com.sdl.selenium.web.table.Table;
 import org.fasttrackit.util.TestBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,18 +16,20 @@ public class ExamplesTest extends TestBase{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestBase.class);
 
-    static WebLocator firstName =  new WebLocator().setText("John");
-    static WebLocator lastName = new WebLocator().setText("Moore");
+    Table table = new Table().setClasses("table-striped");
 
-    static WebLocator email = new WebLocator().setText("nickwhite@mail.com");
-    static WebLocator row = new WebLocator().setTag("tr").setChildNodes(email);
+    WebLocator firstName =  new WebLocator().setText("John");
+    WebLocator lastName = new WebLocator().setText("Moore");
 
-    static CheckBox select = new CheckBox(row);
+    WebLocator email = new WebLocator().setText("nickwhite@mail.com");
+    WebLocator row = new WebLocator().setTag("tr").setChildNodes(email);
+
+    CheckBox select = new CheckBox(row);
 
 
     @Test
     public void selectRowTest() {
-        //doLogin(USER_NAME, PASSWORD);
+        doLogin(USER_NAME, PASSWORD);
 
 
         select.click();
@@ -33,17 +39,51 @@ public class ExamplesTest extends TestBase{
 
         select.click();
         LOGGER.debug(select.getSelector().toString());
+    }
 
+    @Test
+    public void selectRowFromTableTest(){
+        doLogin(USER_NAME, PASSWORD);
+
+       // Table table = new Table().setClasses("table-striped");
+        Row row = table.getRow(2);
+        CheckBox select = new CheckBox(row);
+        select.click();
+
+        Row row2 = table.getRow("nickwhite@mail.com");
+        CheckBox select2 = new CheckBox(row2);
+        select2.click();
+
+        Row row3 = table.getRow("bobsmith@", SearchType.STARTS_WITH);
+        CheckBox select3 = new CheckBox(row3);
+        select3.click();
+
+        Row row4 = table.getRow(new Cell(2, "David"), new Cell(3, "Miller"));
+        CheckBox select4 = new CheckBox(row4);
+        select4.click();
+
+        Row row5 = table.getRow(new Cell("JohnXXXX"), new Cell ("Moore"));
+        CheckBox select5 = new CheckBox(row5);
+        select5.click();
+
+        //Row row6 = table.getRow(new Cell ( "johncarter@mail.com"));
+        //CheckBox select6 = new CheckBox(row6);
+        //select6.click();
 
     }
 
     public static void main(String[] args) {
+      ExamplesTest test= new ExamplesTest();
+    //  System.out.println(test.select.getSelector());
+
+    //  test.row.setChildNodes(test.firstName,test.lastName);
+
+    // LOGGER.debug(test.select.getSelector().toString());
+
+        //Row row4 = test.table.getRow(new Cell(2, "David"), new Cell(3, "Miller"));
+        //CheckBox select4 = new CheckBox(row4);
 
 
-        System.out.println(select.getSelector());
-
-        row.setChildNodes(firstName,lastName);
-
-        LOGGER.debug(select.getSelector().toString());
-    }
+       //LOGGER.debug(select4.getSelector().toString());
+   }
 }
